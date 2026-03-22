c00eb3b94ffc4f300d13dd88dd01d5a78f26b6f5
ravindrasingh <ravindra.singh12701@gmail.com>
2026-03-22 10:02:47 +0530

    added image

--- src/main/java/com/javatechie/dto/User.java
+++ src/main/java/com/javatechie/dto/User.java
@@ -1,15 +1,19 @@
 package com.javatechie.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class User {
 
     private int id;
     private String name;
     private String email;
+
+    public User() {}
+
+    public User(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
 }
